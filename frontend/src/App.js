6b9a2e339720cc5a78c254f3877b10bdb94331ef
Mohit verma <mohit.verma@remotestate.com>
2023-12-06 19:04:43 +0530

    dev-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  console.log("dev");
   return (
     <div>
       <Toaster position="top-right" />
